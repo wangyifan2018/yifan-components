fd07e60f0cb8b926651e3f82a7d04b0dfa9a0f05
Yifan <372104922@qq.com>
2022-04-05 20:12:46 +0800

    feat: add story book

--- src/App.tsx
+++ src/App.tsx
@@ -7,41 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu
-          defaultIndex={'0'}
-          onSelect={(index) => {
-            alert(index);
-          }}
-        >
-          <MenuItem index={'0'}>cool link</MenuItem>
-          <MenuItem index={'1'}>cool link2</MenuItem>
-          <MenuItem index={'2'}>cool link3</MenuItem>
+        <Menu>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link2</MenuItem>
+          <MenuItem>cool link3</MenuItem>
         </Menu>
-        <Button
-          onClick={(e) => {
-            e.preventDefault();
-            alert(123);
-          }}
-        >
-          {' '}
-          Hello{' '}
-        </Button>
-        <Button disabled> Hello </Button>
-        <Button btnType={'primary'} size={'lg'}>
-          Hello
-        </Button>
-        <Button btnType={'danger'} size={'lg'}>
-          Hello
-        </Button>
-        <Button btnType={'link'} href="http://www.baidu.com">
-          {' '}
-          Baidu Link{' '}
-        </Button>
-        <Button btnType={'link'} href="http://www.baidu.com" disabled>
-          {' '}
-          Baidu Link{' '}
-        </Button>
-
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,7 +1,8 @@
-import React, { useState, createContext, CSSProperties } from 'react';
-import classNames from 'classnames';
+import React, { FC, useState, createContext, CSSProperties } from 'react'
+import classNames from 'classnames'
+import { MenuItemProps } from './menuItem'
 
-type MenuMode = 'horizontal' | 'vertical';
+type MenuMode = 'horizontal' | 'vertical'
 export interface MenuProps {
   /**默认 active 的菜单项的索引值 */
   defaultIndex?: string;
@@ -18,41 +19,60 @@ interface IMenuContext {
   index: string;
   onSelect?: (selectedIndex: string) => void;
   mode?: MenuMode;
-  defaultOpenSubMenus?: string[];
+  defaultOpenSubMenus?: string[];  
 }
 
-export const MenuContext = createContext<IMenuContext>({ index: '0' });
-
-const Menu: React.FC<MenuProps> = (props) => {
-  const { className, mode, style, children, defaultIndex, onSelect } = props;
-  const [currentActive, setActive] = useState(defaultIndex);
-  const classes = classNames('yifan-menu', className, {
+export const MenuContext = createContext<IMenuContext>({index: '0'})
+/**
+ * 为网站提供导航功能的菜单。支持横向纵向两种模式，支持下拉菜单。
+ * ~~~js
+ * import { Menu } from 'vikingship'
+ * ~~~
+ */
+export const Menu: FC<MenuProps> = (props) => {
+  const { className, mode, style, children, defaultIndex, onSelect, defaultOpenSubMenus } = props
+  const [ currentActive, setActive ] = useState(defaultIndex)
+  const classes = classNames('viking-menu', className, {
     'menu-vertical': mode === 'vertical',
     'menu-horizontal': mode !== 'vertical',
-  });
+  })
   const handleClick = (index: string) => {
-    setActive(index);
-    if (onSelect) {
-      onSelect(index);
+    setActive(index)
+    if(onSelect) {
+      onSelect(index)
     }
-  };
+  }
   const passedContext: IMenuContext = {
     index: currentActive ? currentActive : '0',
     onSelect: handleClick,
     mode,
-  };
+    defaultOpenSubMenus,
+  }
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<MenuItemProps>
+      const { displayName } = childElement.type
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
+        return React.cloneElement(childElement, {
+          index: index.toString()
+        })
+      } else {
+        console.error("Warning: Menu has a child which is not a MenuItem component")
+      }
+    })
+  }
   return (
-    <ul className={classes} style={style}>
+    <ul className={classes} style={style} data-testid="test-menu">
       <MenuContext.Provider value={passedContext}>
-        {children}
+        {renderChildren()}
       </MenuContext.Provider>
     </ul>
-  );
-};
-
+  )
+}
 Menu.defaultProps = {
   defaultIndex: '0',
   mode: 'horizontal',
-};
+  defaultOpenSubMenus: [],
+}
 
 export default Menu;
