d24b7752c271ae19cb7bb76393fc3aea8bc75d49
Yifan <372104922@qq.com>
2022-04-17 15:51:26 +0800

    feat: 下拉菜单交互

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -20,11 +20,44 @@ const SubMenu: React.FC<SubMenuProps> = ({
   children,
   className,
 }) => {
+  const [menuOpen, setOpen] = useState(false);
   const context = useContext(MenuContext);
   const classes = classNames('menu-item submenu-item', className, {
     'is-active': context.index === index,
   });
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggle);
+    }, 300);
+  };
+  const clickEvents =
+    context.mode === 'vertical'
+      ? {
+          onClick: handleClick,
+        }
+      : {};
+  const hoverEvents =
+    context.mode !== 'vertical'
+      ? {
+          onMouseEnter: (e: React.MouseEvent) => {
+            handleMouse(e, true);
+          },
+          onMouseLeave: (e: React.MouseEvent) => {
+            handleMouse(e, false);
+          },
+        }
+      : {};
   const renderChildren = () => {
+    const subMenuClasses = classNames('viking-submenu', {
+      'menu-opened': menuOpen,
+    });
     const childComponent = React.Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>;
       if (true) {
@@ -34,11 +67,13 @@ const SubMenu: React.FC<SubMenuProps> = ({
         console.error('Warning: SubMenu has a child is not a MenuItem');
       }
     });
-    return <ul className="viking-submenu">{childComponent}</ul>;
+    return <ul className={subMenuClasses}>{childComponent}</ul>;
   };
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">{title}</div>
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
+        {title}
+      </div>
       {renderChildren()}
     </li>
   );
