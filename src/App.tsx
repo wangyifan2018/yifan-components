b364374e92fb02bd08f17b5996a2ba833f199490
Yifan <372104922@qq.com>
2022-04-18 17:27:10 +0800

    chore: 验证transition，添加wrapper属性

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import Button from './components/Button';
+import Transition from './components/Transition/transition';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
 import SubMenu from './components/Menu/subMenu';
 import Icon from './components/Icon/icon';
+
 library.add(fas);
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -28,17 +31,27 @@ function App() {
           </SubMenu>
           <MenuItem>cool link3</MenuItem>
         </Menu>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button size="lg" onClick={() => setShow(!show)}>
+          Toggle
+        </Button>
+        <Transition in={show} timeout={300} animation="zoom-in-left">
+          <div>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>{' '}
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>{' '}
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+          </div>
+        </Transition>
+        <Transition in={show} timeout={300} animation="zoom-in-left" wrapper>
+          <Button btnType="primary" size="lg">
+            A Large Button
+          </Button>
+        </Transition>
       </header>
     </div>
   );
